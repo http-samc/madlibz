a3f2f9913e927bf1a5b06e8248c058819dc0edef
http-samc <67826352+http-samc@users.noreply.github.com>
2021-09-02 09:36:33 -0500

    added initial components of testRequest()

--- app/src/main/java/dev/smrth/madlibz/MainActivity.java
+++ app/src/main/java/dev/smrth/madlibz/MainActivity.java
@@ -3,6 +3,18 @@ package dev.smrth.madlibz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +22,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.testRequest();
+    }
+
+    public void testRequest() {
+        final TextView text = findViewById(R.id.text);
+
+        RequestQueue q = Volley.newRequestQueue(this);
+        String url = "http://madlibz.herokuapp.com/api/random";
+
+        JsonObjectRequest req = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        text.setText(response.toString());
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        return;
+                    }
+                });
     }
 }
