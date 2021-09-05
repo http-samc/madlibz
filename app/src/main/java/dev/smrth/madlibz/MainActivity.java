eb537fefc3518c5fecd5843d939e605f5c5cc3c8
therealbaljeet <chitgopekarsamarth@gmail.com>
2021-09-04 19:36:35 -0500

    added basic dynamic madlib generatio

--- app/src/main/java/dev/smrth/madlibz/MainActivity.java
+++ app/src/main/java/dev/smrth/madlibz/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.android.volley.NetworkResponse;
@@ -19,22 +22,28 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     private final String MADLIB_API_URL = "https://madlibz.herokuapp.com/api/random";
+    private final int BLANKS_START = 69420;
+    private final int TITLE = 42069;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //this.getMadlib();
+        this.getMadlib();
     }
 
     /**
      * Makes a GET request to MainActivity.MADLIB_API_URL to get
      * a JSONObject representation of a Madlib.
      * Calls MainActivity.renderMadlib() to display it.
+     * Called on load (if no previous Madlib)
      */
+
     public void getMadlib() {
 
         RequestQueue requestQueue = Volley.newRequestQueue(this);
@@ -55,19 +64,60 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
+     * Calls getMadlib() when the refresh_madlib
+     * button is clicked. Calls okToRefresh() before proceeding.
+     * @param v View representing the button that was clicked
+     */
+    public void refreshMadlib(View v) {
+        // TODO write okToRefresh() and use in conditional here
+        this.destroyMadlib();
+        this.getMadlib();
+    }
+
+    /**
      * Renders a Madlib onto the main activity,
      * called by MainActivity.getMadlib()
      * @param madlib JSONObject representing a Madlib
      */
     public void renderMadlib(JSONObject madlib) {
-        final TextView madlibContainer = findViewById(R.id.text);
+
+        // Getting refernce to Madlib Container
+        final LinearLayout madlibContianerLL = findViewById(R.id.madlib_container);
+
+        // Creating title TV and adding to Madlib Container
+        final TextView madlibTitleTV = new TextView(this);
+        madlibTitleTV.setId(this.TITLE);
+        madlibContianerLL.addView(madlibTitleTV);
 
         try {
-            String title = madlib.getString("title");
-            madlibContainer.setText(title);
+            // Set title
+            madlibTitleTV.setText(madlib.getString("title"));
+
+            // Get Blanks and Values
+            JSONArray blanks =  madlib.getJSONArray("blanks");
+            JSONArray values =  madlib.getJSONArray("value"); // set in SP for later; 2 longer than blanks
+
+
+            /*
+                Create blanks programmatically & dynamically
+                ID -> "blank_<idx of blank>"
+                Placeholder -> "<blanks.getString(<idx of blank>)"
+             */
+
+            for (int i = 0; i < blanks.length(); i++) {
+                EditText current = new EditText(this);
+                current.setHint(blanks.getString(i));
+                current.setId(this.BLANKS_START+i);
+                madlibContianerLL.addView(current);
+            }
         }
         catch (JSONException e) {
-            Log.w("CHITGOPEKAR", "JSON key not found!");
+            Log.w("CHITGOPEKAR", e.toString());
         }
+    }
+
+    public void destroyMadlib() {
+        final LinearLayout madlibContianerLL = findViewById(R.id.madlib_container);
+        madlibContianerLL.removeAllViews();
     }
 }
